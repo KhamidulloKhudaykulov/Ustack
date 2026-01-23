b00ca1a8f9947607e804f745fe0cce56073e7c18
KhamidulloKhudaykulov <xudaykulov123@gmail.com>
2026-01-23 15:15:55 +0500

    updated controllers

---
+++ src/Server/UStack.Api/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UStack.Identity.Domain.Outcome;
+
+namespace UStack.Api.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected IActionResult Problem(Error error)
+        {
+            return Problem(
+                title: error.Code,
+                detail: error.Message,
+                statusCode: StatusCodes.Status400BadRequest
+            );
+        }
+
+    }
+}
